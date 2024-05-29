90f7be321a6edfb6e45aa80df222858b3b46d4b6
Fernando Depizol <luisfernando.depizol@gmail.com>
2024-05-29 00:02:16 -0300

    Alterado para enviar certificado SSL e login e senha na requisição

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,34 @@
 
 
 
+import javax.net.ssl.SSLContext;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Invocation.Builder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 
+import org.glassfish.jersey.SslConfigurator;
+import org.glassfish.jersey.client.authentication.HttpAuthenticationFeature;
+
 public class Main {
 
 	public static void main(String[] args) {
+		
+		SslConfigurator config = SslConfigurator
+				.newInstance()
+				.trustStoreFile("./server.keystore")
+				.trustStorePassword("livraria");
+		
+		SSLContext context = config.createSSLContext();
+		
+		Client client = ClientBuilder.newBuilder().sslContext(context).build();
+		
+		HttpAuthenticationFeature auth = HttpAuthenticationFeature.basic("admin", "password");
+		client.register(auth);
 	
-		Livros livros = ClientBuilder.newClient()
-				.target("http://localhost:8080/livraria-virtual")
+		Livros livros = client
+				.target("https://localhost:8443/livraria-virtual")
 				.path("livro")
 				.request(MediaType.APPLICATION_XML)
 				.get(Livros.class);
--- src/main/java/MainPost.java
+++ src/main/java/MainPost.java
@@ -1,22 +1,30 @@
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 public class MainPost {
 
 	public static void main(String[] args) {
 		
-		Livro livro = new Livro(4L, "Livro C", "ISBN-8888", "Genero A", 19.90, "Autor 1");
-		Livro livroResposta = ClientBuilder.newClient()
+		Livro livro = new Livro(6L, "Livro C", "ISBN-8887", "Genero A", 19.90, "Autor 1");
+		Response response = ClientBuilder.newClient()
 				.target("http://localhost:8080/livraria-virtual")
 				.path("livro")
 				.request()
-				.post(Entity.entity(livro, MediaType.APPLICATION_XML), Livro.class);
+				.post(Entity.xml(livro));
 		
-		
-	
-		
-		System.out.println(livroResposta.getTitulo());
+		if(response.getStatus() == Status.CREATED.getStatusCode()) {
+			ItemBusca item = ClientBuilder.newClient()
+					.target(response.getLocation())
+					.request(MediaType.APPLICATION_XML)
+					.get(ItemBusca.class);
+			System.out.println(item.getLivro().getTitulo());
+		}else if(response.getStatus() >= Status.BAD_REQUEST.getStatusCode()) {
+			System.out.println("Verifique se todos os dados estão corretos.");
+		}
+
 
 	}
 
